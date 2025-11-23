640b5955eed3e2e5ffa38c9c66109385d97389b1
Abdul Aliyev <aaa7c4@gmail.com>
2025-11-23 01:09:07 -0500

    Implement reminders handling MVP backend

---
+++ types.ts
@@ -0,0 +1,20 @@
+export interface Contact {
+  mode: "email" | "sms";
+  address: string;
+}
+
+export interface Reminder {
+  id?: number;
+  title: string;
+  date: string;
+  location?: any;
+  description: string;
+  reminders: Contact[];
+  alerts: number[];
+  is_recurring: boolean;
+  recurrence?: string;
+  start_date?: string;
+  end_date?: string;
+  last_alert_time?: Date | null;
+  is_active?: boolean;
+}
